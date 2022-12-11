db9e796e010fe4a9ac802192899a0a04dd2d5a23
canasmh <canasmh@yahoo.com>
2022-12-11 00:32:03 -0500

    Create a container for welcome content

--- client/src/pages/Home/Welcome.js
+++ client/src/pages/Home/Welcome.js
@@ -3,15 +3,17 @@ import React from "react";
 export default function Welcome(props) {
     return (
         <section className="welcome">
-            <h1 className="welcome__heading">
-                <span className="welcome__heading--span-1">Foothills </span>
-                <span className="welcome__heading--span-2">Property Group</span>
-            </h1>
-            <p className="welcome__description">
-                <span className="welcome__description--span-1">Real Estate</span>
-                <span className="welcome__description--span-2">Experts</span>
-            </p>
-            <input type="search" placeholder="Search by zip code, city, or neighborhood" />
+            <div className="welcome__container">
+                <h1 className="welcome__heading">
+                    <span className="welcome__heading--span-1">Foothills </span>
+                    <span className="welcome__heading--span-2">Property Group</span>
+                </h1>
+                <p className="welcome__description">
+                    <span className="welcome__description--span-1">Real Estate </span>
+                    <span className="welcome__description--span-2">Experts</span>
+                </p>
+                <input type="search" placeholder="Search by zip code, city, or neighborhood" className="welcome__search" />
+            </div>
         </section>
     )
 }
