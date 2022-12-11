9d6512bb4f61c0648531395d574d837458960b21
canasmh <canasmh@yahoo.com>
2022-12-11 03:05:38 -0500

    Import about component into Home component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Welcome from "./Home/Welcome";
+import About from "./Home/About";
 
 export default function Home(props) {
 
     return (
-        <Welcome />
+        <>
+            <Welcome />
+            <main>
+                <About />
+            </main>
+        </>
     )
 
 }
