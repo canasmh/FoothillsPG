f830531390c537cc001329bbecfa4e483032f6fe
canasmh <canasmh@yahoo.com>
2022-12-13 22:52:35 -0500

    Restore testimonials section

---
+++ client/src/pages/Home/Testimonials.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+export default function Testimonials(props) {
+
+    return (
+        <section className="testimonials">
+
+            <h2 className="u-margin-bottom-l">What our clients have to say</h2>
+
+            <div className="testimonials__container">
+                <div className="testimonials__container--individual">
+                    <div className="testimonials__card">
+                        <div className="testimonials__img-container">
+                            <img src="./images/profile-pic-1.jpeg" className="testimonials__img" />
+                        </div>
+                        <div className="testimonials__text-container">
+                            <h3 className="testimonials__heading">Juan was fantastic</h3>
+                            <p className="testimonials__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo sed egestas egestas fringilla phasellus faucibus. Cursus turpis massa tincidunt dui.</p>
+                        </div>
+                    </div>
+                    <p className="testimonials__link">View on Zillow  &rarr;</p>
+                </div>
+
+                <div className="testimonials__container--individual">
+                    <div className="testimonials__card">
+                        <div className="testimonials__img-container">
+                            <img src="./images/profile-pic-2.jpeg" className="testimonials__img" />
+                        </div>
+                        <div className="testimonials__text-container">
+                            <h3 className="testimonials__heading">Best decision ever</h3>
+                            <p className="testimonials__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo sed egestas egestas fringilla phasellus faucibus. Cursus turpis massa tincidunt dui.</p>
+                        </div>
+                    </div>
+                    <p className="testimonials__link">View on Zillow &rarr;</p>
+                </div>
+
+                <div className="testimonials__container--individual">
+                    <div className="testimonials__card">
+                        <div className="testimonials__img-container">
+                            <img src="./images/profile-pic-3.jpeg" className="testimonials__img" />
+                        </div>
+                        <div className="testimonials__text-container">
+                            <h3 className="testimonials__heading"> 100% recommend</h3>
+                            <p className="testimonials__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo sed egestas egestas fringilla phasellus faucibus. Cursus turpis massa tincidunt dui.</p>
+                        </div>
+                    </div>
+                    <p className="testimonials__link">View on Zillow &rarr;</p>
+                </div>
+            </div>
+            <a className="btn">See all reviews</a>
+        </section>
+    );
+}
