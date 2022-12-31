010e533a12646f883c6c61a32a3b83a05943f74c
canasmh <canasmh@yahoo.com>
2022-12-31 11:11:54 -0500

    Animate navbar on scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,3 +10,42 @@ navWithDropdown.addEventListener('click', () => {
     }
 })
 
+const navBar = document.querySelector('nav');
+const navBarLogo = document.querySelector('.nav__logo');
+const navBarHeight = navBar.offsetHeight;
+console.log(navBarHeight);
+window.onscroll = () => scrollFunction();
+let scrollingUp = false;
+const scrollFunction = (event) => {
+    const elemScrollTop = document.documentElement.scrollTop;
+    const docScrollTop = document.body.scrollTop;
+
+    if (docScrollTop > (navBarHeight * 1.5) || elemScrollTop > (navBarHeight * 1.5) ) {
+        console.log('gt')
+        navBar.style.maxHeight = '8rem';
+        navBar.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+        navBar.style.transition = 'top 0.5s ease';
+        navBarLogo.style.maxHeight = '8rem';
+        navBar.style.top= "0";
+        scrollingUp = true;
+    } else {
+        console.log('lt');
+
+        if (scrollingUp) {
+            navBar.style.transition = 'top 0.2s ease';
+            scrollingUp = false;
+        } else {
+            navBar.style.transition == 'none';
+        }
+        
+        console.log(Math.max(elemScrollTop, docScrollTop))
+        navBar.style.top = `-${Math.max(elemScrollTop, docScrollTop)}px`
+        navBar.style.maxHeight = '10rem';
+        navBarLogo.style.maxHeight = '10rem';
+        navBar.style.backgroundColor = `rgba(0, 0, 0, ${0.8 * (1 - (elemScrollTop / navBarHeight))})`
+        
+    } 
+    console.log('scrollingUp', scrollingUp);
+}
+
+
